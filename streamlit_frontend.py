0a089bebd77d9e4b5598f632ea4444eaa1faa834
Suraj Chaudhary <surajchaudhary1436@outlook.com>
2026-07-01 09:59:59 +0530

    Output streaming functionality added.

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -4,6 +4,19 @@ from langchain_core.messages import HumanMessage
 
 CONFIG = {'configurable': {"thread_id": "thread_1"}}
 
+# Helper function to extract 'plain text' from content blocks produced by 'streaming' mode of chatbot
+def extract_text(content):
+    # content can be a plain string OR a list of content block dicts
+    if isinstance(content, str):
+        return content
+    elif isinstance(content, list):
+        return "".join(
+            block.get("text", "")
+            for block in content
+            if isinstance(block, dict) and block.get("type") == "text"
+        )
+    return ""
+
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
 
@@ -21,9 +34,27 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config = CONFIG)
-    ai_message = response['messages'][-1].content[0]["text"]
+    
     # add the assistant's response to the message history
-    st.session_state['message_history'].append({"role": "assistant", "content": ai_message})
+    # with st.chat_message('assistant'):
+
+    #     ai_message = st.write_stream(
+    #         message_chunk.content for message_chunk, metadata in chatbot.stream(
+    #             {'messages': [HumanMessage(content = user_input)]},
+    #             config = CONFIG,
+    #             stream_mode = 'messages'
+    #         )
+    #     )               Returns list of content blocks instead of plain text
+
+
     with st.chat_message('assistant'):
-        st.text(ai_message)
+        ai_message = st.write_stream(
+            extract_text(message_chunk.content)
+            for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config=CONFIG,
+                stream_mode='messages'
+            )
+        )
+        
+    st.session_state['message_history'].append({"role": "assistant", "content": ai_message})
